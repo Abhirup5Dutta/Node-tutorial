c40956c646fc2f213d519a27f2c4266b4fb71e97
Abhirup5Dutta <abhirupdutta04acse@gmail.com>
2021-11-06 15:29:01 +0530

    First commit

---
+++ 11-fs-async.js
@@ -0,0 +1,32 @@
+const { readFile, writeFile } = require("fs");
+
+console.log("start");
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+
+  readFile("./content/second.txt", "utf-8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        // console.log(result);
+        console.log("done with this task");
+      }
+    );
+  });
+});
+console.log("starting next task");
